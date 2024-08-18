e6a16cdece6c2fbf9dd94e17123eb5d5d662ef47
sood2105 <devansh.sood@gmail.com>
2024-08-18 19:28:09 +0530

    cors modify

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-# from flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app,origins=["*"])
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/api/data', methods=['POST'])
