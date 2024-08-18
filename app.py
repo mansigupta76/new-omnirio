46435d6e11be9d82fc9c7f89bb0c3cbe276951d4
sood2105 <devansh.sood@gmail.com>
2024-08-18 17:25:48 +0530

    Testing

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/api/data', methods=['POST'])
+def handle_data():
+    if request.method == 'POST':
+        data = request.json
+        email = data.get("email")
+        # Process data here
+        return jsonify({ "data": email})
+    else:
+        return jsonify({"message": "Send a POST request to this endpoint"})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5001)
